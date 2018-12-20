60c48a8a2735fff005503655fa9f175618565340
mdmilev <mihail.milev@arcusglobal.com>
2018-12-20 14:28:56 +0000

    Update config.js

--- website/js/config.js
+++ website/js/config.js
@@ -1,11 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_WDohFluux', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '48i1rm1atjbl5jqo9lmkr11ai8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-west-1_qf0h7c0B3', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '66hhmibsavc913qs0toor0bl6t', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://rrmr83vw5l.execute-api.eu-west-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://thwllqoirl.execute-api.eu-west-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
         email: 'mihail.milev@arcusglobal.com'
